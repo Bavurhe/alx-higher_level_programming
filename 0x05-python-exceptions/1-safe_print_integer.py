3591684cf2ab204260b2e8f6d0b4338ff1a1a1e4
Bavurhe <sorayabav95@gmail.com>
2023-03-28 01:37:35 -0700

    Exception

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -10,7 +10,7 @@ def safe_print_integer(value):
       if a TypeError or ValueError occurs - False.
       Otherwise - True.
     """
-    try: 
+    try:
         print("{:d}".format(value))
         return (True)
     except (TypeError, ValueError):
